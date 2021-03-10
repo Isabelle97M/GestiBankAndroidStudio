94377ee076be7e5ff10bf8cd86f04fc7d353cd2c
Isabelle MATHIEU <isabelle.mathieu@outlook.com>
2021-03-10 17:45:43 +0100

    adding email sending option

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 
 app.use(express.json());
 
+var generator = require('generate-password');
+
+var nodemailer = require('nodemailer');
+
 // mettre le serveur à l'écoute(en marche) sur le port 90
 app.listen(
     90,
@@ -62,9 +66,43 @@ app.get('/customers/validated/list', (req,res) => {
 
 app.post('/customers/add',  async (req,res) => {
     try {
+        // Password Generation 
+        var password = generator.generate({
+                length: 10,
+                numbers: true,
+                uppercase: true
+            });
+
            const newCustomer = req.body
+           newCustomer.password = password
+           email = newCustomer.email
+           password = newCustomer.password
            const addedCustomer = await db.collection('users').insertOne(newCustomer)
            res.status(200).json(addedCustomer)
+
+           var mailOptions = {
+            from: 'essai.mailing.dev@gmail.com',
+            to: email,
+            subject: 'Validation de création de compte GestiBank',
+            text: 'Bienvenue chez GestiBank' + '\n' 
+            + 'Votre gestionnaire de transcation bancaire en ligne. Vous venez de créer un compte de type : '
+            + '\n' + req.body.account + '.'
+            + '\n Vous pouvez désormais vous connecter sur votre espace client grâce à vos identifiants.'
+            + ' \n Votre identifiant est : ' 
+            + email
+            + '\n Votre mot de passe est : ' 
+            + password
+          };
+          
+// SEND VALIDATION E-MAIL : 
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
        } catch (err) {
            console.log(err)
            throw err
@@ -77,6 +115,42 @@ app.post('/customers/add',  async (req,res) => {
         const updateCustomer = req.body
         const customer = await db.collection('users').replaceOne({email, "role" : "CUSTOMER"},updateCustomer)
         res.status(200).json(customer)
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+ })
+
+ app.put('/customers/validationByAgent/:agent',  async (req,res) => {
+    try {
+        const email = req.params.email;
+        const updateCustomer = req.body;
+        const agent = req.body.agent;
+        const customer = await db.collection('users').replaceOne({email, "role" : "CUSTOMER"},updateCustomer)
+        res.status(200).json(customer)
+
+        var mailOptions = {
+            from: 'essai.mailing.dev@gmail.com',
+            to: email,
+            subject: 'Affectation de votre compte GestiBank',
+            text: 'Bonjour,'  
+            + '\n Votre compte a bien été validé par Gestibank.' 
+            + '\n Votre agent est : '
+            + agent  + '.'
+            + 'Pour toutes questions, vous pouvez prendre contact avec votre agent.'
+            + 'Bien cordialement, GestiBank.'
+          };
+          
+// SEND VALIDATION E-MAIL : 
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+// ERROR PUT CUSTOMER VALIDATION REQUEST :
     } catch (err) {
         console.log(err)
         throw err
@@ -182,3 +256,18 @@ app.post('/admins/add',  async (req,res) => {
         throw err
     }
  })
+
+
+
+// Envoi de mails
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'essai.mailing.dev@gmail.com',
+      pass: 'dev1244MI'
+    }
+  });
+  
+
+
+  
